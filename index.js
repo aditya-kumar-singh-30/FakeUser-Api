c9f9a0018c691f8efd609f5e9abcde613ffdf859
Aditya kumar singh <aditya.21scse1280002@galgotiasuniversity.edu.in>
2024-12-31 01:57:39 +0530

    cors issue

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const fs = require("fs");
+const cors = require("cors");
 const users = require("./MOCK_DATA.json");
 const app = express();
 app.use(express.urlencoded({extended : true}));
